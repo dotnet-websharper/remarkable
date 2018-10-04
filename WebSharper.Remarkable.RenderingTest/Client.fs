4b9e51d21e7dcc16e1ceb774f83efbb7f9001f91
Loic Denuziere <loic.denuziere@intellifactory.com>
2018-10-04 15:39:27 +0200

    Update license info

--- WebSharper.Remarkable.RenderingTest/Client.fs
+++ WebSharper.Remarkable.RenderingTest/Client.fs
@@ -1,3 +1,22 @@
+// $begin{copyright}
+//
+// This file is part of WebSharper
+//
+// Copyright (c) 2008-2018 IntelliFactory
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you
+// may not use this file except in compliance with the License.  You may
+// obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied.  See the License for the specific language governing
+// permissions and limitations under the License.
+//
+// $end{copyright}
 namespace WebSharper.Remarkable.RenderingTest
 
 open WebSharper
@@ -22,7 +41,7 @@ module Client =
         remarkableConfig.Linkify <- true
         remarkableConfig.LinkTarget <- ""
         remarkableConfig.Typographer <- false
-        remarkableConfig.Quotes <- "'“”‘’'"
+        remarkableConfig.Quotes <- "'ï¿½ï¿½ï¿½ï¿½'"
 
         let md = new Remarkable.Remarkable(remarkableConfig)
         let rvInput = Var.Create ""
