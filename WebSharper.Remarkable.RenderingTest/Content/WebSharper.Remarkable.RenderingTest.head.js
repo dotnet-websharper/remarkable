bf314dafd81a4afd94a0bc619b7f2097746329d8
Loic Denuziere <loic.denuziere@intellifactory.com>
2018-10-30 16:00:01 +0100

    Remove committed generated files

--- WebSharper.Remarkable.RenderingTest/Content/WebSharper.Remarkable.RenderingTest.head.js
+++
@@ -1,1 +0,0 @@
-document.write("<script src=\"https://cdn.jsdelivr.net/remarkable/1.7.1/remarkable.min.js\" type=\"text/javascript\" charset=\"UTF-8\"></script><!--[if lte IE 9.0]>\r\n<![endif]-->\r\n")
